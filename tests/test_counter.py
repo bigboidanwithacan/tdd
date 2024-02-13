29fba95de8a0c625635d77d327812b4b6c8e782b
Daniel <daniawesomecool@gmail.com>
2024-02-12 21:59:54 -0800

    Last task

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -39,25 +39,40 @@ class CounterTest(TestCase):
         self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
         
     def test_update_a_counter(self):
-        """It should update a counter and increase its value by 1"""
-        create_result = self.client.post('/counters/dan')
-        self.assertEqual(create_result.status_code, 201)
-        baseline_value = create_result.json['dan']
-        update_result = self.client.put('/counters/dan')
-        self.assertEqual(update_result.status_code, 200)
-        updated_value = update_result.json['dan']
-        self.assertEqual(updated_value, baseline_value + 1)
-        result = self.client.put('/counters/unknown')
-        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        """Should successfully update a counter"""
+        result = self.client.post('/counters/test')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(result.json['test'], 0)
+        toCompare = self.client.put('/counters/test')
+        self.assertEqual(toCompare.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.json['test'] + 1, toCompare.json['test'])
         
     def test_get_a_counter(self):
         """It should return the value of the counter"""
-        create_result = self.client.post('/counters/alex')
-        self.assertEqual(create_result.status_code, 201)
-        get_result = self.client.get('/counters/alex')
-        self.assertEqual(get_result.status_code, 200)
-        self.assertEqual(get_result.json['alex'], 0)
+        create_result = self.client.post('/counters/dan')
+        self.assertEqual(create_result.status_code, status.HTTP_201_CREATED)
+        get_result = self.client.get('/counters/dan')
+        self.assertEqual(get_result.status_code, status.HTTP_200_OK)
+        self.assertEqual(get_result.json['dan'], 0)
+        
+    def test_delete_a_counter(self):
+        """It should delete a counter"""
+        create_result = self.client.post('/counters/dan')
+        self.assertEqual(create_result.status_code, status.HTTP_201_CREATED)
+        delete_result = self.client.delete('/counters/dan')
+        self.assertEqual(delete_result.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_delete_a_non_existent_counter(self):
+        """It should return a 404 for a non-existent counter"""
+        result = self.client.delete('/counters/unknown')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        
+    def test_get_a_non_existent_counter(self):
+        """It should return a 404 for a non-existent counter"""
         result = self.client.get('/counters/unknown')
         self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
         
-        
+    def test_update_a_non_existent_counter(self):
+        """It should return a 404 for a non-existent counter"""
+        result = self.client.put('/counters/unknown')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
